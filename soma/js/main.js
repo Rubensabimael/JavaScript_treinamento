e044c345fd66993dd73f651491c46df6e9a64e82
rubensabimael <rubensabimael26@gmail.com>
2021-11-26 15:30:07 -0300

    🐛 finalizada a correção de um bug para que não estava calculando um valor em mais e menos

--- soma/js/main.js
+++ soma/js/main.js
@@ -3,8 +3,21 @@ const botao = document.querySelector('button');
 
 botao.onclick = function () {
     let valor1 = prompt('Digite um valor?');
-    let valor2 = prompt('Digiter um outro valor');
-    let soma = (valor1* valor2);
-    alert("Valor trotal da soma foi de "+soma)
+    
+    const operador = prompt('Digite um operador aritmético ?');
+
+    let valor2 = prompt('Digiter um outro valor?');
+
+    let soma = 0;
+     
+        soma = calculo(valor1, valor2, operador);
+    
+
+    alert("Valor trotal da soma foi de "+soma);
 }
- 
+
+ function calculo (valor1,valor2,operador){
+    return eval( valor1 + operador + valor2)
+}
+
+
