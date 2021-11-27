cf79e92a70e9dc68e728ba43c6c24a39c4522f38
rubensabimael <rubensabimael26@gmail.com>
2021-11-27 17:06:03 -0300

    🎉 implementação de código no html

--- soma/js/main.js
+++ soma/js/main.js
@@ -3,21 +3,21 @@ const botao = document.querySelector('button');
 
 botao.onclick = function () {
     let valor1 = prompt('Digite um valor?');
-    
+
     const operador = prompt('Digite um operador aritmético ?');
 
     let valor2 = prompt('Digiter um outro valor?');
 
     let soma = 0;
-     
-        soma = calculo(valor1, valor2, operador);
-    
 
-    alert("Valor trotal da soma foi de "+soma);
-}
+    soma = calculo(valor1, valor2, operador);
 
- function calculo (valor1,valor2,operador){
-    return eval( valor1 + operador + valor2)
-}
+
+    alert("Valor trotal da soma foi de " + soma);
+};
+
+function calculo(valor1, valor2, operador) {
+    return eval(valor1 + operador + valor2)
+};
 
 
